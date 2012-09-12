1a1b71115fd64b53ef0c3f60f1ff9464c87fa4cd
David Boon <david.boon@gmail.com>
2012-09-12 15:51:30 -0400

    Revert "Changing Underscore's _.each semantics to whitelist for arrays and arguments, instead of a numeric length propery."
    This reverts commit 63053dbb972c360e2c6874d7294a4a06ebc26f88.

--- underscore.js
+++ underscore.js
@@ -80,7 +80,7 @@
     if (obj == null) return;
     if (nativeForEach && obj.forEach === nativeForEach) {
       obj.forEach(iterator, context);
-    } else if (_.isArray(obj) || _.isArguments(obj)) {
+    } else if (obj.length === +obj.length) {
       for (var i = 0, l = obj.length; i < l; i++) {
         if (iterator.call(context, obj[i], i, obj) === breaker) return;
       }
@@ -344,8 +344,7 @@
 
   // Return the number of elements in an object.
   _.size = function(obj) {
-    var hasLength = _.isArray(obj) || _.isArguments(obj) || _.isString(obj);
-    return hasLength ? obj.length : _.keys(obj).length;
+    return (obj.length === +obj.length) ? obj.length : _.keys(obj).length;
   };
 
   // Array Functions
